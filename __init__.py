d77519dc865fced3d55c33efcd1e1483a9a05c19
Adi Eyal <adi@clearforest.io>
2023-08-01 23:30:29 +0200

    Bumped version to 0.1.1

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from comfy_dynamicprompts.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
